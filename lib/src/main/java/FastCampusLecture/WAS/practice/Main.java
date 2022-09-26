7c3568a9a33d6cb18b05a0c60d9d7d1fd807f970
leedg92 <49174093+leedg92@users.noreply.github.com>
2022-09-26 22:46:45 +0900

    123123

--- lib/src/main/java/FastCampusLecture/WAS/practice/Main.java
+++ lib/src/main/java/FastCampusLecture/WAS/practice/Main.java
@@ -2,7 +2,7 @@ package FastCampusLecture.WAS.practice;
 
 import java.io.IOException;
 
-//GET /calculate?operand1=11&operator=*&operand2=55
+//GET /calculate?operand1=11&operator=*&operand2=55aaa
 public class Main {
 	public static void main(String[] args) throws IOException {
 		new CustomWebApplicationServer(8080).start();
